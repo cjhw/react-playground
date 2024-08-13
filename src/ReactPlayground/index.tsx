2a7aff8e690b3b51e1c4faaad0ed76c5a73c0625
cjh <1784581164@qq.com>
2024-08-14 01:31:57 +0800

    feat: error message and theme

--- src/ReactPlayground/index.tsx
+++ src/ReactPlayground/index.tsx
@@ -3,9 +3,21 @@ import 'allotment/dist/style.css';
 import Header from "./components/Header";
 import CodeEditor from "./components/CodeEditor";
 import Preview from "./components/Preview";
+import { useContext } from "react";
+import { PlaygroundContext } from "./PlaygroundContext";
+
+import './index.scss';
 
 export default function ReactPlayground() {
-    return <div style={{height: '100vh'}}>
+
+    const { 
+        theme, 
+    } = useContext(PlaygroundContext)
+
+    return <div 
+        className={theme}
+        style={{height: '100vh'}}
+    >
         <Header/>
         <Allotment defaultSizes={[100, 100]}>
             <Allotment.Pane minSize={0}>
