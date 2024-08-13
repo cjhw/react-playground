6ca484b12e75b23b4b3a4e74691115357bb52bf7
cjh <1784581164@qq.com>
2024-08-14 01:36:14 +0800

    feat: share link

--- src/ReactPlayground/components/Header/index.tsx
+++ src/ReactPlayground/components/Header/index.tsx
@@ -3,10 +3,13 @@ import styles from './index.module.scss'
 import logoSvg from './icons/logo.svg';
 import { useContext } from 'react';
 import { PlaygroundContext } from '../../PlaygroundContext';
-import { MoonOutlined, SunOutlined } from '@ant-design/icons';
+import { DownloadOutlined, MoonOutlined, ShareAltOutlined, SunOutlined } from '@ant-design/icons';
+import { message } from 'antd';
+import copy from 'copy-to-clipboard';
+import { downloadFiles } from '../../utils';
 
 export default function Header() {
-  const { theme, setTheme} = useContext(PlaygroundContext)
+  const { files, theme, setTheme} = useContext(PlaygroundContext)
 
   return (
     <div className={styles.header}>
@@ -29,6 +32,20 @@ export default function Header() {
             onClick={() => setTheme('light')}
           />
         )}
+        <ShareAltOutlined 
+          style={{marginLeft: '10px'}}
+          onClick={() => {
+            copy(window.location.href);
+            message.success('分享链接已复制。')
+          }}
+        />
+        <DownloadOutlined 
+          style={{marginLeft: '10px'}}
+          onClick={async () => {
+            await downloadFiles(files);
+            message.success('下载完成')
+          }}
+        />
       </div>
     </div>
   )
